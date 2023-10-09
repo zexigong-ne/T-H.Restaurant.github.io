a8601fa12755f5b8676fdbb8aeefab8278fb92a6
username <useremail>
2023-10-09 16:49:45 -0700

    seven

--- backend.js
+++ backend.js
@@ -6,9 +6,11 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static("public"));
+
 app.use("/rountes", apiRounter);
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+  console.log(`listening on port ${PORT}`);
 });
 
