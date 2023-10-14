d2b613b8aa59e94a555ff3189a3d259d82eeec35
PengTao <129124105+tpwxpk120@users.noreply.github.com>
2023-10-14 15:18:23 -0700

    Add routeuer into app js

--- app.js
+++ app.js
@@ -3,11 +3,10 @@ import bodyParser from "body-parser";
 import router from "./routes/routes.js";
 import path from "path";
 import { fileURLToPath } from "url";
-
+import routeruser from "./routes/routesuser.js";
 
 const app = express();
 const port = 8000;
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,8 +17,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(router);
-
-
+app.use(routeruser);
 
 app.listen(port, () => {
   console.log("Server is running on port ${port}");
